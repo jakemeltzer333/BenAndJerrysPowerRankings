99379d8d36aa77d61ab49e462b2d31df7fb75650
Jacob Meltzer <jmeltzer33@gmail.com>
2017-09-06 14:22:44 -0400

    make flavors in seeds file

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,12 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+
+# Categories
+
+regular = Category.create!(name: 'Regular')
+fro_yo = Category.create!(name: 'Fro-Yo')
+core = Category.create!(name: 'Core')
+non_dairy = Category.create!(name: 'Non-Dairy')
+
+# Flavors
